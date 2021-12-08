25870b891db0b462ec7c0bdbdfd465ae590affe5
Viviana <vivi.plc95@hotmail.com>
2021-12-08 18:23:53 -0500

    funds converter logic

---
+++ FinalProjectSoftware/Converters/FundsConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FinalProjectSoftware.Converters
+{
+    [ValueConversion(typeof(double), typeof(Brush))]
+    class FundsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (double.TryParse(value.ToString(), out double funds))
+            {
+                if (funds is < 5000 or > 20000)
+                {
+                    return Brushes.LightGray;
+                }
+                else
+                {
+                    return funds is >= 5000 and < 10000 or >= 15000 and <= 20000 ? Brushes.LightSalmon : Brushes.LightGreen;
+                }
+            }
+            else
+            {
+                return Brushes.White;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
